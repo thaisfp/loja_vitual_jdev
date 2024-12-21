2acd59e4a530f0836fffad84580afba103fa54b1
Thais Ferreira Passos <thispassos55@gmail.com>
2024-12-21 17:23:18 -0300

    feat: estrutura da aplicacao

---
+++ src/main/java/com/example/loja_vitual_jdev/LojaVitualJdevApplication.java
@@ -0,0 +1,13 @@
+package com.example.loja_vitual_jdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVitualJdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVitualJdevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/loja_vitual_jdev/LojaVitualJdevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loja_vitual_jdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVitualJdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
