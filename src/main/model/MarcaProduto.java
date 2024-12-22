36baaebbc52e585fbcde598c19cfd67d9473b61c
Thais Ferreira Passos <thispassos55@gmail.com>
2024-12-22 11:13:47 -0300

    feat: conexao com banco

---
+++ src/main/model/MarcaProduto.java
@@ -0,0 +1,21 @@
+import java.io.Serializable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "marca_produto")
+@SequenceGenerator(name = "seq_marca_prodto", sequenceName = "seq_marca_prodto", allocationSize = 1, initialValue = 1)
+public class MarcaProduto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_prodto")
+    private Long id;
+
+    private String nomeDesc;
+}
